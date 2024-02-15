b2e332a9dc7c8b6d3b0ec267dcd2f342a559e9eb
Vraj Mohan <vraj@codeforamerica.org>
2024-02-14 17:07:35 -0800

    Use radio buttons for sensitive questions in PDF

--- src/test/java/org/mdbenefits/app/pdf/PdfServiceTest.java
+++ src/test/java/org/mdbenefits/app/pdf/PdfServiceTest.java
@@ -25,19 +25,25 @@ class PDFServiceTest {
     @Test
     void generate() throws IOException {
         Submission submission = new SubmissionTestBuilder()
-            .withPersonalInfo("John", "Doe", "10", "12", "1999",
-                "", "", "", "", "111111111")
-            .withHouseholdMember("Jane", "Doe", "1", "5", "2000",
-                "wife", "F", "", "", "222222222", null, null)
-            .with("homeAddressStreetAddress1", "972 Mission St, 5th Floor")
-            .with("homeAddressCity", "San Francisco")
-            .with("homeAddressState", "CA")
-            .with("homeAddressZipCode", "94103")
-            .with("phoneNumber", "(510) 555-1212")
-            .with("signature", "John Hancock")
-            .with("someoneInHHIsPregnant", "true")
-            .with("someoneHasDrugKingpinFelony", "false")
-            .build();
+                .withPersonalInfo("John", "Doe", "10", "12", "1999",
+                        "", "", "", "", "111111111")
+                .withHouseholdMember("Jane", "Doe", "1", "5", "2000",
+                        "wife", "F", "", "", "222222222", null, null)
+                .with("homeAddressStreetAddress1", "972 Mission St, 5th Floor")
+                .with("homeAddressCity", "San Francisco")
+                .with("homeAddressState", "CA")
+                .with("homeAddressZipCode", "94103")
+                .with("phoneNumber", "(510) 555-1212")
+                .with("signature", "John Hancock")
+                .with("someoneInHHIsPregnant", "true")
+                .with("someoneHasDrugKingpinFelony", "false")
+                .with("someoneHasVolumeDrugDealerFelony", "true")
+                .with("someoneHasSexualOffenceConviction", "false")
+                .with("someoneIsViolatingParole", "true")
+                .with("someoneConvictedForLyingAboutBenefits", "false")
+                .with("someoneConvictedForTradingBenefits", "true")
+                .with("someoneIsReceivingBenefitsWithFakeID", "false")
+                .build();
         submission.setFlow("mdBenefitsFlow");
 
         File pdfFile = pdfService.generate(submission);
