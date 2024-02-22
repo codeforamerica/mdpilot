4661a6054e223e7b6716a4882ddf9621c92caaf7
Ana Medrano Fernandez <amedrano@codeforamerica.org@Anas-MacBook-Pro-2.local>
2024-02-22 12:47:06 -0800

    Basic condition that allows for exclusion

--- src/main/java/org/mdbenefits/app/submission/conditions/DisplayPregnancyQuestion.java
+++ src/main/java/org/mdbenefits/app/submission/conditions/DisplayPregnancyQuestion.java
@@ -4,10 +4,10 @@ import formflow.library.data.Submission;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DisplayPregnancyQuestion extends BasicCondition {
+public class DisplayPregnancyQuestion extends BasicExclusionCondition {
 
     @Override
     public Boolean run(Submission submission) {
-        return (run(submission, "applicantSex", "Other") || run(submission, "applicantSex", "F"));
+        return run(submission, "applicantSex", "M");
     }
 }
