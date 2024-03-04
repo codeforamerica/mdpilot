b34353239a5dc74a130d56be1c2463109e2c302e
Bethany Seeger <bseeger@codeforamerica.org>
2024-03-04 17:07:01 -0500

    Adds better error handling and deletes duplicate directories.

--- src/main/java/org/mdbenefits/app/cli/GoogleDriveClient.java
+++ src/main/java/org/mdbenefits/app/cli/GoogleDriveClient.java
@@ -1,13 +1,17 @@
 package org.mdbenefits.app.cli;
 
+import com.google.api.services.drive.model.File;
+import java.util.Map;
 import java.util.List;
 
 public interface GoogleDriveClient {
 
     // returns string ID of new folder
-    String createFolder(String locationId, String folderName, List<String> errors);
+    String createFolder(String locationId, String folderName, Map<String, String> errors);
 
-    String uploadFile(String folderId, String filename, String mimeType, byte[] data, List<String> errors);
-    
-    List<String> findDirectory(String name, String parentId);
+    String uploadFile(String folderId, String filename, String mimeType, byte[] data, Map<String, String> errors);
+
+    List<File> findDirectory(String name, String parentId);
+
+    boolean deleteDirectory(String name, String directoryId, Map<String, String> errors);
 }
