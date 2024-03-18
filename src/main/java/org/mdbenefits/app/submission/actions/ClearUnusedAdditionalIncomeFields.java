4b4378b58bd83eebc3330fc3355fe7d3083c8b85
Bethany Seeger <bseeger@codeforamerica.org>
2024-03-18 16:11:22 -0400

    Add a few tests for the clear additional income action.
    This moves the action to the end of the amount page as well.

--- src/main/java/org/mdbenefits/app/submission/actions/ClearUnusedAdditionalIncomeFields.java
+++ src/main/java/org/mdbenefits/app/submission/actions/ClearUnusedAdditionalIncomeFields.java
@@ -17,13 +17,12 @@ public class ClearUnusedAdditionalIncomeFields implements Action {
      * This action will clear out the unused additional income amount fields from the submission's input data, if the choice for
      * additional income type is no longer in the income type list.
      *
-     * @param formSubmission form submission object the action is associated with, not null
-     * @param submission     submission object the action is associated with, not null
+     * @param submission submission object the action is associated with, not null
      */
     @Override
-    public void run(FormSubmission formSubmission, Submission submission) {
+    public void run(Submission submission) {
         Map<String, String> submissionInputData = (Map) submission.getInputData();
-        List<String> additionalIncomeTypes = (List) formSubmission.getFormData().getOrDefault("additionalIncome[]", List.of());
+        List<String> additionalIncomeTypes = (List) submission.getInputData().getOrDefault("additionalIncome[]", List.of());
         boolean none = additionalIncomeTypes.contains("NONE");
 
         for (AdditionalIncomeType type : AdditionalIncomeType.values()) {
---
+++ src/test/java/org/mdbenefits/app/submission/actions/ClearUnusedAdditionalIncomeFieldsTest.java
@@ -0,0 +1,72 @@
+package org.mdbenefits.app.submission.actions;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import formflow.library.data.Submission;
+import java.util.List;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mdbenefits.app.data.SubmissionTestBuilder;
+import org.mdbenefits.app.data.enums.AdditionalIncomeType;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@ActiveProfiles("test")
+@SpringBootTest
+class ClearUnusedAdditionalIncomeFieldsTest {
+
+    private ClearUnusedAdditionalIncomeFields clearUnusedAdditionalIncomeFields;
+
+    @BeforeEach
+    void setup() {
+        clearUnusedAdditionalIncomeFields = new ClearUnusedAdditionalIncomeFields();
+    }
+
+    @Test
+    public void shouldClearAllFieldsIfNoneSelected() {
+        Submission submission = new SubmissionTestBuilder()
+                .with("additionalIncome[]", List.of("NONE"))
+                .with(AdditionalIncomeType.ALIMONY.getInputFieldName(), "2000")
+                .with(AdditionalIncomeType.PENSION_RETIREMENT.getInputFieldName(), "222")
+                .with(AdditionalIncomeType.VETERANS_BENEFITS.getInputFieldName(), "1000")
+                .build();
+        submission.setFlow("mdBenefitsFlow");
+
+        clearUnusedAdditionalIncomeFields.run(submission);
+
+        assertThat(submission.getInputData().get(AdditionalIncomeType.ALIMONY.getInputFieldName())).isNull();
+        assertThat(submission.getInputData().get(AdditionalIncomeType.PENSION_RETIREMENT.getInputFieldName())).isNull();
+        assertThat(submission.getInputData().get(AdditionalIncomeType.VETERANS_BENEFITS.getInputFieldName())).isNull();
+    }
+
+    @Test
+    public void shouldClearAllFieldsIfDifferentOnesSelected() {
+        Submission submission = new SubmissionTestBuilder()
+                .with("additionalIncome[]", List.of(
+                        AdditionalIncomeType.ALIMONY.name(),
+                        AdditionalIncomeType.FRIENDS_FAMILY_CONTRIBUTION.name(),
+                        AdditionalIncomeType.OTHER.name()))
+                .with(AdditionalIncomeType.ALIMONY.getInputFieldName(), "2000")
+                .with(AdditionalIncomeType.FRIENDS_FAMILY_CONTRIBUTION.getInputFieldName(), "1000")
+                .with(AdditionalIncomeType.OTHER.getInputFieldName(), "500")
+                // pretend these below were there before, they should get cleared out, since they
+                // are not in the list
+                .with(AdditionalIncomeType.PENSION_RETIREMENT.getInputFieldName(), "222")
+                .with(AdditionalIncomeType.WORKERS_COMPENSATION.getInputFieldName(), "111")
+                .with(AdditionalIncomeType.VETERANS_BENEFITS.getInputFieldName(), "1000")
+                .build();
+        submission.setFlow("mdBenefitsFlow");
+
+        clearUnusedAdditionalIncomeFields.run(submission);
+
+        assertThat(submission.getInputData().get(AdditionalIncomeType.ALIMONY.getInputFieldName())).isEqualTo("2000");
+        assertThat(submission.getInputData().get(AdditionalIncomeType.FRIENDS_FAMILY_CONTRIBUTION.getInputFieldName())).isEqualTo(
+                "1000");
+        assertThat(submission.getInputData().get(AdditionalIncomeType.OTHER.getInputFieldName())).isEqualTo("500");
+
+        // these should have been cleared out.
+        assertThat(submission.getInputData().get(AdditionalIncomeType.PENSION_RETIREMENT.getInputFieldName())).isNull();
+        assertThat(submission.getInputData().get(AdditionalIncomeType.WORKERS_COMPENSATION.getInputFieldName())).isNull();
+        assertThat(submission.getInputData().get(AdditionalIncomeType.VETERANS_BENEFITS.getInputFieldName())).isNull();
+    }
+}
