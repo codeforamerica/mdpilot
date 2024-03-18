072907a6f35848f07002f90a6d6392c777145b62
Bethany Seeger <bseeger@codeforamerica.org>
2024-03-18 15:39:01 -0400

    Map additionalIncome into the PDF. Take none value out of additionalIncome enum.

--- src/main/java/org/mdbenefits/app/data/enums/AdditionalIncomeType.java
+++ src/main/java/org/mdbenefits/app/data/enums/AdditionalIncomeType.java
@@ -1,25 +1,39 @@
 package org.mdbenefits.app.data.enums;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
 
 @Getter
 public enum AdditionalIncomeType {
 
-    ALIMONY("additional-income.choice.alimony"),
-    CHILD_SUPPORT("additional-income.choice.child-support"),
-    FRIENDS_FAMILY_CONTRIBUTION("additional-income.choice.friends-and-family"),
-    PENSION_RETIREMENT("additional-income.choice.pension-retirement"),
-    SUPPLEMENTAL_SECURITY_INCOME("additional-income.choice.ssi"),
-    SOCIAL_SECURITY("additional-income.choice.ss"),
-    UNEMPLOYMENT("additional-income.choice.unemployment"),
-    VETERANS_BENEFITS("additional-income.choice.veterans-benefits"),
-    WORKERS_COMPENSATION("additional-income.choice.workers-comp"),
-    OTHER("additional-income.choice.other"),
-    NONE_OF_THESE("additional-income.choice.none");
+    ALIMONY("additionalIncomeAlimony", "additional-income.choice.alimony"),
+    CHILD_SUPPORT("additionalIncomeChildSupport", "additional-income.choice.child-support"),
+    FRIENDS_FAMILY_CONTRIBUTION("additionalIncomeFriendsAndFamily", "additional-income.choice.friends-and-family"),
+    PENSION_RETIREMENT("additionalIncomePensionRetirement", "additional-income.choice.pension-retirement"),
+    SUPPLEMENTAL_SECURITY_INCOME("additionalIncomeSSI", "additional-income.choice.ssi"),
+    SOCIAL_SECURITY("additionalIncomeSS", "additional-income.choice.ss"),
+    UNEMPLOYMENT("additionalIncomeUnemployment", "additional-income.choice.unemployment"),
+    VETERANS_BENEFITS("additionalIncomeVeteransBenefits", "additional-income.choice.veterans-benefits"),
+    WORKERS_COMPENSATION("additionalIncomeWorkersComp", "additional-income.choice.workers-comp"),
+    OTHER("additionalIncomeOther", "additional-income.choice.other");
 
+    private final String inputFieldName;
     private final String labelSrc;
+    private static final Map<String, AdditionalIncomeType> ENUM_BY_NAME = new HashMap<>();
 
-    AdditionalIncomeType(String labelSrc) {
+    static {
+        for (AdditionalIncomeType type : AdditionalIncomeType.values()) {
+            ENUM_BY_NAME.put(type.name(), type);
+        }
+    }
+
+    AdditionalIncomeType(String inputFieldName, String labelSrc) {
         this.labelSrc = labelSrc;
+        this.inputFieldName = inputFieldName;
+    }
+
+    public static AdditionalIncomeType getEnumByName(String name) {
+        return ENUM_BY_NAME.get(name);
     }
 }
