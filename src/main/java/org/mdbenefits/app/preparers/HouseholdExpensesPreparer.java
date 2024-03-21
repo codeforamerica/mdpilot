47e85a87910b1829c2d08db63d5269906f47fd2a
Ana Medrano Fernandez <amedrano@codeforamerica.org@Anas-MacBook-Pro-2.local>
2024-03-21 16:28:41 -0700

    Remove last references for utility and update household expenses prepared

--- src/main/java/org/mdbenefits/app/preparers/HouseholdExpensesPreparer.java
+++ src/main/java/org/mdbenefits/app/preparers/HouseholdExpensesPreparer.java
@@ -11,62 +11,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
+import org.mdbenefits.app.data.enums.HomeExpensesType;
 import org.springframework.stereotype.Component;
 
 @Component
 @Slf4j
 public class HouseholdExpensesPreparer implements SubmissionFieldPreparer {
 
-    private static final Map<String, String> EXPENSES = new HashMap<>();
-
-    static {
-        EXPENSES.put("householdHomeExpenseAmount_wildcard_rent", "Rent");
-        EXPENSES.put("householdHomeExpenseAmount_wildcard_mortgage", "Mortgage");
-        EXPENSES.put("householdHomeExpenseAmount_wildcard_homeownerInsurance", "Homeowner's Insurance");
-        EXPENSES.put("householdHomeExpenseAmount_wildcard_propertyTax", "Property Tax");
-        EXPENSES.put("householdHomeExpenseAmount_wildcard_condominiumFees", "Condo Fees");
-        EXPENSES.put("householdHomeExpenseAmount_wildcard_lotRent", "Lot Rental");
-        EXPENSES.put("householdHomeExpenseAmount_wildcard_floodInsurance", "Flood Insurance");
-    }
-
     @Override
     public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, PdfMap pdfMap) {
         Map<String, SubmissionField> results = new HashMap<>();
 
-        var householdExpenses = (List) submission.getInputData().getOrDefault("householdHomeExpenses[]", emptyList());
+        var householdExpenses = (List<String>) submission.getInputData().getOrDefault("householdHomeExpenses[]", emptyList());
         if (!householdExpenses.isEmpty()) {
-            List<String> sortedExpenses = EXPENSES.keySet().stream().sorted().toList();
             int i = 1;
-            for (String expense : sortedExpenses) {
-                var expenseInput = submission.getInputData().get(expense);
-                if (expenseInput != null) {
-                    results.put("householdExpensesType" + i, new SingleField("householdExpensesType", EXPENSES.get(expense), i));
-                    results.put("householdExpensesAmount" + i,
-                            new SingleField("householdExpensesAmount", (String) expenseInput, i));
-                    results.put("householdExpensesFreq" + i, new SingleField("householdExpensesFreq", "Monthly", i));
-                    i++;
+            for (String expense : householdExpenses) {
+                var householdExpenseEnum = HomeExpensesType.getEnumByName(expense);
+                if (householdExpenseEnum != null) {
+                    String frequencyFieldName = householdExpenseEnum.getInputFieldName() + "Frequency";
+                    results.put(frequencyFieldName, new SingleField(frequencyFieldName, "Monthly", null));
                 }
-            }
-
-            // Check for "Other" - theres only one spot for this in the PDF so just combine them
-            var totalOtherAmount = 0.0;
-            try {
-                var expensesOther = submission.getInputData().get("householdHomeExpenseAmount_wildcard_otherHomeExpenses");
-                if (expensesOther != null) {
-                    totalOtherAmount += Double.parseDouble((String) expensesOther);
-                }
-            } catch (NumberFormatException e) {
-                // We should have validations in place for this already
-                log.warn("Could not parse amount", e);
-            }
-
-            if (totalOtherAmount > 0) {
-                results.put("Other", new SingleField("Other", "Yes", null));
-
-                results.put("householdExpensesType_" + i, new SingleField("householdExpensesType_" + i, "Other", null));
-                results.put("householdExpensesAmount_" + i,
-                        new SingleField("householdExpensesAmount_" + i, Double.toString(totalOtherAmount), null));
-                results.put("householdExpensesFreq_" + i, new SingleField("householdExpensesFreq_" + i, "Monthly", null));
             }
         }
 
--- src/test/java/org/mdbenefits/app/preparers/HouseholdExpensesPreparerTest.java
+++ src/test/java/org/mdbenefits/app/preparers/HouseholdExpensesPreparerTest.java
@@ -26,7 +26,7 @@ class HouseholdExpensesPreparerTest {
     }
 
     @Test
-    public void shouldAddFieldsForHouseholdExpenses() {
+    public void shouldAddFieldsForHouseholdExpensesFrequency() {
         Submission submission = new SubmissionTestBuilder()
             .with("householdHomeExpenses[]", List.of("RENT", "GARBAGE", "PHONE"))
             .with("homeExpenseRent", "20")
@@ -36,26 +36,14 @@ class HouseholdExpensesPreparerTest {
 
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
 
-        assertThat(result.size()).isEqualTo(9);
-        assertThat(result.get("householdExpensesType1"))
-            .isEqualTo(new SingleField("householdExpensesType", "Homeowner's Insurance", 1));
-        assertThat(result.get("householdExpensesAmount1"))
-            .isEqualTo(new SingleField("homeExpenseGarbage", "30", null));
-        assertThat(result.get("householdExpensesFreq1"))
-            .isEqualTo(new SingleField("householdExpensesFreq", "Monthly", null));
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get("homeExpenseRentFrequency"))
+            .isEqualTo(new SingleField("homeExpenseRentFrequency", "Monthly", null));
 
-        assertThat(result.get("householdExpensesType2"))
-            .isEqualTo(new SingleField("householdExpensesType", "Mortgage", null));
-        assertThat(result.get("householdExpensesAmount2"))
-            .isEqualTo(new SingleField("homeExpenseRent", "20", null));
-        assertThat(result.get("householdExpensesFreq2"))
-            .isEqualTo(new SingleField("householdExpensesFreq", "Monthly", null));
+        assertThat(result.get("homeExpenseGarbageFrequency"))
+            .isEqualTo(new SingleField("homeExpenseGarbageFrequency", "Monthly", null));
 
-        assertThat(result.get("householdExpensesType2"))
-            .isEqualTo(new SingleField("householdExpensesType", "Mortgage", null));
-        assertThat(result.get("householdExpensesAmount2"))
-            .isEqualTo(new SingleField("homeExpensePhone", "23", null));
-        assertThat(result.get("householdExpensesFreq2"))
-            .isEqualTo(new SingleField("householdExpensesFreq", "Monthly", null));
+        assertThat(result.get("homeExpensePhoneFrequency"))
+            .isEqualTo(new SingleField("homeExpensePhoneFrequency", "Monthly", null));
     }
 }
