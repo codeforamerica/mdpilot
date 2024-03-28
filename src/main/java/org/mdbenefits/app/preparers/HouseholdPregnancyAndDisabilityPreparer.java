3a81c4f2304d9b65854d498eb17cf09ebe869a4b
Ana Medrano Fernandez <amedrano@codeforamerica.org@Anas-MacBook-Pro-2.local>
2024-03-28 15:46:50 -0700

    Combine applicant and household pregnancy and disability logic for preparer

--- src/main/java/org/mdbenefits/app/preparers/HouseholdPregnancyAndDisabilityPreparer.java
+++ src/main/java/org/mdbenefits/app/preparers/HouseholdPregnancyAndDisabilityPreparer.java
@@ -5,10 +5,14 @@ import formflow.library.pdf.PdfMap;
 import formflow.library.pdf.SingleField;
 import formflow.library.pdf.SubmissionField;
 import formflow.library.pdf.SubmissionFieldPreparer;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
+
+import static java.util.Collections.emptyList;
+
+import org.mdbenefits.app.utils.SubmissionUtilities;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -18,30 +22,57 @@ public class HouseholdPregnancyAndDisabilityPreparer implements SubmissionFieldP
     public Map<String, SubmissionField> prepareSubmissionFields(Submission submission, PdfMap pdfMap) {
         Map<String, SubmissionField> results = new HashMap<>();
 
-        var household = (List<Map<String, Object>>) submission.getInputData().get("household");
+        ArrayList<String> householdMembersPregnant = new ArrayList<String>();
+        ArrayList<String> householdMembersWithDisability = new ArrayList<String>();
 
-        if (household != null) {
-            boolean hasPregnantMember = household.stream().anyMatch(member -> 
-                    member.getOrDefault("householdMemberIsPregnant","").toString().equalsIgnoreCase("Yes"));
-            
-            boolean hasDisabledMember = household.stream().anyMatch(member ->
-                    member.getOrDefault("householdMemberIsPregnant","").toString().equalsIgnoreCase("Yes"));
-            
-            results.put("householdMemberIsPregnant", new SingleField("householdMemberIsPregnant",
-                    hasPregnantMember ? "Yes" : "No", null));
-            if (hasPregnantMember) {
-                results.put("householdMemberIsPregnantSeeCover", 
-                    new SingleField("householdMemberIsPregnantSeeCover", "See cover page", null));
-            }
-            
-            results.put("householdMemberHasDisability", new SingleField("householdMemberHasDisability",
-                    hasDisabledMember ? "Yes" : "No", null));
-            if (hasDisabledMember) {
-                results.put("householdMemberHasDisabilitySeeCover",
-                    new SingleField("householdMemberHasDisabilitySeeCover", "See cover page",null));
+        var inputData = submission.getInputData();
+        if (inputData.getOrDefault("isApplicantPregnant", "No").toString().equalsIgnoreCase("Yes")) {
+            householdMembersPregnant.add(SubmissionUtilities.applicantFullNameFormatted(submission));
+        }
+
+        if (inputData.getOrDefault("applicantHasDisability", "No").toString().equalsIgnoreCase("Yes")) {
+            householdMembersWithDisability.add(SubmissionUtilities.applicantFullNameFormatted(submission));
+        }
+
+        var household = (List<Map<String, Object>>) inputData.getOrDefault("household", emptyList());
+
+        if (!household.isEmpty()) {
+            householdMembersPregnant.addAll(householdMembersWithCondition(household, "householdMemberIsPregnant"));
+            householdMembersWithDisability.addAll(householdMembersWithCondition(household, "householdMemberHasDisability"));
+        }
+
+        results.put("householdHasDisability",
+            new SingleField("householdHasDisability", !householdMembersWithDisability.isEmpty() ? "Yes" : "No", null));
+        results.put("householdHasPregnancy",
+            new SingleField("householdHasPregnancy", !householdMembersPregnant.isEmpty() ? "Yes" : "No", null));
+
+        if (!householdMembersWithDisability.isEmpty()) {
+            results.put("householdHasDisabilitySeeCover",
+                new SingleField("householdHasDisabilitySeeCover", "See cover page", null));
+            for (int i = 0; (i < 5 && i < householdMembersWithDisability.size()); i++) {
+                var index = i + 1;
+                results.put("householdDisabilityName" + index,
+                    new SingleField("householdDisabilityName" + index, householdMembersWithDisability.get(i), null));
             }
         }
-        
+
+        if (!householdMembersPregnant.isEmpty()) {
+            results.put("householdHasPregnancySeeCover",
+                new SingleField("householdHasPregnancySeeCover", "See cover page", null));
+            for (int i = 0; (i < 5 && i < householdMembersPregnant.size()); i++) {
+                var index = i + 1;
+                results.put("householdPregnancyName" + index,
+                    new SingleField("householdPregnancyName" + index, householdMembersPregnant.get(i), null));
+            }
+        }
+
         return results;
+    }
+
+
+    private List<String> householdMembersWithCondition(List<Map<String, Object>> householdSubflow, String fieldName) {
+        return householdSubflow.stream()
+            .filter(hhMember -> "yes".equalsIgnoreCase(String.valueOf(hhMember.get(fieldName))))
+            .map(hhMember -> SubmissionUtilities.householdMemberFullNameFormatted(hhMember)).toList();
     }
 }
--- src/test/java/org/mdbenefits/app/preparers/HouseholdPregnancyAndDisabilityPreparerTest.java
+++ src/test/java/org/mdbenefits/app/preparers/HouseholdPregnancyAndDisabilityPreparerTest.java
@@ -14,34 +14,173 @@ class HouseholdPregnancyAndDisabilityPreparerTest {
     private final HouseholdPregnancyAndDisabilityPreparer preparer = new HouseholdPregnancyAndDisabilityPreparer();
 
     @Test
-    public void shouldMapYesToSeeCoverPage() {
+    public void pregnancyListCutsOffAtFive(){
         Submission submission = new SubmissionTestBuilder()
-            .withHouseholdMemberApplying("Betty", "White", "10", "2", "1999", "Child", "F", "NeverMarried", "firstGrade",
-                "123456789", "Yes", "Yes", null, null)
+            .withApplicantWithPregnancy("Alice", "Test")
+            .withHouseholdMemberWithPregnancy("Benny", "Test")
+            .withHouseholdMemberWithPregnancy("Carla", "Test")
+            .withHouseholdMemberWithPregnancy("Damian", "Test")
+            .withHouseholdMemberWithPregnancy("Eli", "Test")
+            .withHouseholdMemberWithPregnancy("Frankie", "Test")
             .build();
+
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
-        assertThat(result.get("householdMemberIsPregnant")).isEqualTo(
-            new SingleField("householdMemberIsPregnant", "Yes", null));
-        assertThat(result.get("householdMemberIsPregnantSeeCover")).isEqualTo(
-            new SingleField("householdMemberIsPregnantSeeCover", "See cover page", null));
-        assertThat(result.get("householdMemberHasDisability")).isEqualTo(
-            new SingleField("householdMemberHasDisability", "Yes", null));
-        assertThat(result.get("householdMemberHasDisabilitySeeCover")).isEqualTo(
-            new SingleField("householdMemberHasDisabilitySeeCover", "See cover page", null));
+        assertThat(result.get("householdHasPregnancy")).isEqualTo(new SingleField("householdHasPregnancy", "Yes", null));
+        assertThat(result.get("householdHasPregnancySeeCover")).isEqualTo(new SingleField("householdHasPregnancySeeCover", "See cover page", null));
+        assertThat(result.get("householdHasDisability")).isEqualTo(new SingleField("householdHasDisability", "No", null));
+        assertThat(result.get("householdHasDisabilitySeeCover")).isNull();
+
+        assertThat(result.get("householdPregnancyName1")).isEqualTo(
+            new SingleField("householdPregnancyName1", "Test, Alice", null));
+        assertThat(result.get("householdPregnancyName2")).isEqualTo(
+            new SingleField("householdPregnancyName2", "Test, Benny", null));
+        assertThat(result.get("householdPregnancyName3")).isEqualTo(
+            new SingleField("householdPregnancyName3", "Test, Carla", null));
+        assertThat(result.get("householdPregnancyName4")).isEqualTo(
+            new SingleField("householdPregnancyName4", "Test, Damian", null));
+        assertThat(result.get("householdPregnancyName5")).isEqualTo(
+            new SingleField("householdPregnancyName5", "Test, Eli", null));
+        assertThat(result.get("householdPregnancyName6")).isNull();
+    }
+
+    @Test
+    public void disabilityListCutsOffAtFive(){
+        Submission submission = new SubmissionTestBuilder()
+            .withApplicantWithDisability("Alice", "Test")
+            .withHouseholdMemberWithDisability("Benny", "Test")
+            .withHouseholdMemberWithDisability("Carla", "Test")
+            .withHouseholdMemberWithDisability("Damian", "Test")
+            .withHouseholdMemberWithDisability("Eli", "Test")
+            .withHouseholdMemberWithDisability("Frankie", "Test")
+            .build();
+
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+        assertThat(result.get("householdHasDisability")).isEqualTo(new SingleField("householdHasDisability", "Yes", null));
+        assertThat(result.get("householdHasDisabilitySeeCover")).isEqualTo(new SingleField("householdHasDisabilitySeeCover", "See cover page", null));
+        assertThat(result.get("householdHasPregnancy")).isEqualTo(new SingleField("householdHasPregnancy", "No", null));
+        assertThat(result.get("householdHasPregnancySeeCover")).isNull();
+
+        assertThat(result.get("householdDisabilityName1")).isEqualTo(
+            new SingleField("householdDisabilityName1", "Test, Alice", null));
+        assertThat(result.get("householdDisabilityName2")).isEqualTo(
+            new SingleField("householdDisabilityName2", "Test, Benny", null));
+        assertThat(result.get("householdDisabilityName3")).isEqualTo(
+            new SingleField("householdDisabilityName3", "Test, Carla", null));
+        assertThat(result.get("householdDisabilityName4")).isEqualTo(
+            new SingleField("householdDisabilityName4", "Test, Damian", null));
+        assertThat(result.get("householdDisabilityName5")).isEqualTo(
+            new SingleField("householdDisabilityName5", "Test, Eli", null));
+        assertThat(result.get("householdDisabilityName6")).isNull();
+    }
+
+    @Test
+    public void properlyMapsDataWhenOnlyApplicantIsPregnancy(){
+        Submission submission = new SubmissionTestBuilder()
+            .withApplicantWithPregnancy("Alice", "Test")
+            .build();
+
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+        assertThat(result.size()).isEqualTo(4);
+        assertThat(result.get("householdHasPregnancy")).isEqualTo(new SingleField("householdHasPregnancy", "Yes", null));
+        assertThat(result.get("householdHasPregnancySeeCover")).isEqualTo(new SingleField("householdHasPregnancySeeCover", "See cover page", null));
+        assertThat(result.get("householdHasDisability")).isEqualTo(new SingleField("householdHasDisability", "No", null));
+        assertThat(result.get("householdHasDisabilitySeeCover")).isNull();
+
+        assertThat(result.get("householdPregnancyName1")).isEqualTo(
+            new SingleField("householdPregnancyName1", "Test, Alice", null));
+        assertThat(result.get("householdPregnancyName2")).isNull();
+    }
+
+    @Test
+    public void properlyMapsDataWhenOnlyApplicantHasDisability(){
+        Submission submission = new SubmissionTestBuilder()
+            .withApplicantWithDisability("Alice", "Test")
+            .build();
+
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+        assertThat(result.size()).isEqualTo(4);
+        assertThat(result.get("householdHasDisability")).isEqualTo(new SingleField("householdHasDisability", "Yes", null));
+        assertThat(result.get("householdHasDisabilitySeeCover")).isEqualTo(new SingleField("householdHasDisabilitySeeCover", "See cover page", null));
+        assertThat(result.get("householdHasPregnancy")).isEqualTo(new SingleField("householdHasPregnancy", "No", null));
+        assertThat(result.get("householdHasPregnancySeeCover")).isNull();
+
+        assertThat(result.get("householdDisabilityName1")).isEqualTo(
+            new SingleField("householdDisabilityName1", "Test, Alice", null));
+        assertThat(result.get("householdDisabilityName2")).isNull();
+    }
+
+    @Test
+    public void properlyMapsWhenApplicantIsNotPregnant(){
+        Submission submission = new SubmissionTestBuilder()
+            .withHouseholdMemberWithPregnancy("Benny", "Test")
+            .withHouseholdMemberWithPregnancy("Carla", "Test")
+            .withHouseholdMemberWithPregnancy("Damian", "Test")
+            .withHouseholdMemberWithPregnancy("Eli", "Test")
+            .withHouseholdMemberWithPregnancy("Frankie", "Test")
+            .build();
+
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+        assertThat(result.size()).isEqualTo(8);
+        assertThat(result.get("householdHasPregnancy")).isEqualTo(new SingleField("householdHasPregnancy", "Yes", null));
+        assertThat(result.get("householdHasPregnancySeeCover")).isEqualTo(new SingleField("householdHasPregnancySeeCover", "See cover page", null));
+        assertThat(result.get("householdHasDisability")).isEqualTo(new SingleField("householdHasDisability", "No", null));
+        assertThat(result.get("householdHasDisabilitySeeCover")).isNull();
+
+
+        assertThat(result.get("householdPregnancyName1")).isEqualTo(
+            new SingleField("householdPregnancyName1", "Test, Benny", null));
+        assertThat(result.get("householdPregnancyName2")).isEqualTo(
+            new SingleField("householdPregnancyName2", "Test, Carla", null));
+        assertThat(result.get("householdPregnancyName3")).isEqualTo(
+            new SingleField("householdPregnancyName3", "Test, Damian", null));
+        assertThat(result.get("householdPregnancyName4")).isEqualTo(
+            new SingleField("householdPregnancyName4", "Test, Eli", null));
+        assertThat(result.get("householdPregnancyName5")).isEqualTo(
+            new SingleField("householdPregnancyName5", "Test, Frankie", null));
+    }
+
+    @Test
+    public void properlyMapsWhenApplicantDoesNotHaveDisability(){
+        Submission submission = new SubmissionTestBuilder()
+            .withHouseholdMemberWithDisability("Benny", "Test")
+            .withHouseholdMemberWithDisability("Carla", "Test")
+            .withHouseholdMemberWithDisability("Damian", "Test")
+            .withHouseholdMemberWithDisability("Eli", "Test")
+            .withHouseholdMemberWithDisability("Frankie", "Test")
+            .build();
+
+        Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
+        assertThat(result.size()).isEqualTo(8);
+        assertThat(result.get("householdHasDisability")).isEqualTo(new SingleField("householdHasDisability", "Yes", null));
+        assertThat(result.get("householdHasDisabilitySeeCover")).isEqualTo(new SingleField("householdHasDisabilitySeeCover", "See cover page", null));
+        assertThat(result.get("householdHasPregnancy")).isEqualTo(new SingleField("householdHasPregnancy", "No", null));
+        assertThat(result.get("householdHasPregnancySeeCover")).isNull();
+
+
+        assertThat(result.get("householdDisabilityName1")).isEqualTo(
+            new SingleField("householdDisabilityName1", "Test, Benny", null));
+        assertThat(result.get("householdDisabilityName2")).isEqualTo(
+            new SingleField("householdDisabilityName2", "Test, Carla", null));
+        assertThat(result.get("householdDisabilityName3")).isEqualTo(
+            new SingleField("householdDisabilityName3", "Test, Damian", null));
+        assertThat(result.get("householdDisabilityName4")).isEqualTo(
+            new SingleField("householdDisabilityName4", "Test, Eli", null));
+        assertThat(result.get("householdDisabilityName5")).isEqualTo(
+            new SingleField("householdDisabilityName5", "Test, Frankie", null));
     }
     
     @Test
-    public void shouldMapNoToBlank() {
+    public void whenNoOneIsPregnantOrHasDisability() {
         Submission submission = new SubmissionTestBuilder()
             .withHouseholdMemberApplying("Betty", "White", "10", "2", "1999", "Child", "F", "NeverMarried", "firstGrade",
                 "123456789", "No", "No", null, null)
             .build();
         Map<String, SubmissionField> result = preparer.prepareSubmissionFields(submission, null);
-        assertThat(result.get("householdMemberIsPregnant")).isEqualTo(
-            new SingleField("householdMemberIsPregnant", "No", null));
-        assertThat(result.get("householdMemberIsPregnantSeeCover")).isNull();
-        assertThat(result.get("householdMemberHasDisability")).isEqualTo(
-            new SingleField("householdMemberHasDisability", "No", null));
-        assertThat(result.get("householdMemberHasDisabilitySeeCover")).isNull();
+        assertThat(result.get("householdHasPregnancy")).isEqualTo(
+            new SingleField("householdHasPregnancy", "No", null));
+        assertThat(result.get("householdHasPregnancySeeCover")).isNull();
+        assertThat(result.get("householdHasDisability")).isEqualTo(
+            new SingleField("householdHasDisability", "No", null));
+        assertThat(result.get("householdHasDisabilitySeeCover")).isNull();
     }
 }
