8d2e1de1d740865c53b3e1cae6c092f3cdc52e05
Bethany Seeger <bseeger@codeforamerica.org>
2024-05-17 16:20:59 -0400

    Revamp select appliation info pages.

--- src/main/java/org/mdbenefits/app/data/enums/ApplicantObjective.java
+++ src/main/java/org/mdbenefits/app/data/enums/ApplicantObjective.java
@@ -5,17 +5,16 @@ import lombok.Getter;
 @Getter
 public enum ApplicantObjective {
 
-  NEED_HELP_IN_OTHER_LANGUAGE("select-app.help-in-other-language"),
-  RE_CERTIFYING_BENEFITS("select-app.re-certifying-benefits"),
-  COLLEGE_STUDENT_IN_APP("select-app.college-student-in-home"),
-  VETERAN_IN_APP("select-app.veteran-in-app"),
-  HELP_WITH_HEALTHCARE("select-app.help-with-healthcare"),
-  AUTHORIZED_REP_FOR_SOMEONE("select-app.authorized-rep"),
-  OTHER("select-app.other");
+    NEED_HELP_IN_OTHER_LANGUAGE("select-app.help-in-other-language"),
+    COLLEGE_STUDENT_IN_APP("select-app.college-student-in-home"),
+    VETERAN_IN_APP("select-app.veteran-in-app"),
+    HELP_WITH_HEALTHCARE("select-app.help-with-healthcare"),
+    AUTHORIZED_REP_FOR_SOMEONE("select-app.authorized-rep"),
+    OTHER("select-app.other");
 
-  private final String labelSrc;
+    private final String labelSrc;
 
-  ApplicantObjective(String labelSrc) {
-    this.labelSrc = labelSrc;
-  }
+    ApplicantObjective(String labelSrc) {
+        this.labelSrc = labelSrc;
+    }
 }
