1c72c1329fa7e74fd40913da5178178d79cb8911
Bethany Seeger <bseeger@codeforamerica.org>
2024-05-20 10:53:56 -0400

    Cleanup the code a little.

--- src/main/java/org/mdbenefits/app/data/enums/Counties.java
+++ src/main/java/org/mdbenefits/app/data/enums/Counties.java
@@ -1,45 +1,33 @@
 package org.mdbenefits.app.data.enums;
 
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Getter;
 
 @Getter
 public enum Counties {
 
-    BALTIMORE("county.option.baltimore_county"),
-    QUEEN_ANNES("county.option.queenannes_county"),
-    OTHER("county.option.other");
+    BALTIMORE("county.option.baltimore_county", true),
+    QUEEN_ANNES("county.option.queenannes_county", true),
+    OTHER("county.option.other", false);
 
-    /*
-    // we may need these again if we switch gears to include them.
-    // we may also want to switch back to hard coded text like below.
-    ALLEGANY("Allegany County"),
-    ANNE_ARUNDEL("Anne Arundel County"),
-    BALTIMORE_CITY("Baltimore City"),
-    CALVERT("Calvert County"),
-    CAROLINE("Caroline County"),
-    CARROLL("Carroll County"),
-    CECIL("Cecil County"),
-    CHARLES("Charles County"),
-    DORCHESTER("Dorchester County"),
-    FREDERICK("Frederick County"),
-    GARRETT("Garrett County"),
-    HARFORD("Harford County"),
-    HOWARD("Howard County"),
-    KENT("Kent County"),
-    MONTGOMERY("Montgomery County"),
-    PRINCE_GEORGES("Prince George's County"),
-    SOMERSET("Somerset County"),
-    ST_MARYS("St. Mary's County"),
-    TALBOT("Talbot County"),
-    WASHINGTON("Washington County"),
-    WICOMICO("Wicomico County"),
-    WORCESTER("Worcester County");
-     */
+    static private final Map<String, Counties> MAP_BY_ENUM_NAME = new HashMap<>();
 
-    private final String labelSrc;
-
-    Counties(String labelSrc) {
-        this.labelSrc = labelSrc;
+    static {
+        for (Counties county : Counties.values()) {
+            MAP_BY_ENUM_NAME.put(county.name(), county);
+        }
     }
 
+    private final String labelSrc;
+    private final boolean inPilot;
+
+    Counties(String labelSrc, boolean inPilot) {
+        this.labelSrc = labelSrc;
+        this.inPilot = inPilot;
+    }
+
+    public static Counties getCountyByName(String name) {
+        return MAP_BY_ENUM_NAME.get(name);
+    }
 }
